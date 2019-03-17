b2992fc8d8742196b1f51926c11cce1ee4223382
Paul Kim <pykim0591@gmail.com>
2019-03-16 22:41:58 -0600

    Add middleware

---
+++ src/middleware/index.ts
@@ -0,0 +1,6 @@
+// import { takeLatest } from 'redux-saga';
+// import { registerUser } from './sagas';
+
+// export function* apiMiddleware() {
+//   yield takeLatest('REGISTER_USER', registerUser);
+// }
