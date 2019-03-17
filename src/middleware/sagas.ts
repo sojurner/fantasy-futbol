1a5ad3084488a25f3f95bae339d0f1cc996a90b4
Paul Kim <pykim0591@gmail.com>
2019-03-16 22:42:06 -0600

    Add redux sagas

---
+++ src/middleware/sagas.ts
@@ -0,0 +1,11 @@
+// import { put } from 'redux-saga/effects';
+// import * as apiCall from '../utils/apicalls';
+
+// export function* registerUser(props) {
+//   yield put({
+//     type: 'IS_LOADING',
+//     payload: true
+//   });
+
+//   // const data = yield apiCall.registerUser();
+// }
