f6a635d10c37047dabafe989cf594893e3165d2e
Paul Kim <paul.kim@catalystrms.com>
2019-04-13 17:39:31 -0600

    Add RequestPlayers fetch call

--- src/utils/apiCalls.ts
+++ src/utils/apiCalls.ts
@@ -1,12 +1,11 @@
 export const registerUser = async userFields => {
-  const { name, username, password, email } = userFields;
+  const { fullName, username, password, email } = userFields;
   const userHash = {
     user_name: username,
-    full_name: name,
+    full_name: fullName,
     password,
     email
   };
-
   const options = {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
@@ -15,11 +14,20 @@ export const registerUser = async userFields => {
 
   try {
     const response = await fetch(
-      'https://fantasy-futbol-api.herokuapp.com/',
+      'http://localhost:3000/api/v1/signup',
       options
     );
     return await response.json();
   } catch (err) {
     return { err };
+  }
+};
+
+export const requestPlayers = async () => {
+  try {
+    const response = await fetch('http://localhost:3000/api/v1/player_stats');
+    return await response.json();
+  } catch (error) {
+    return { error };
   }
 };
